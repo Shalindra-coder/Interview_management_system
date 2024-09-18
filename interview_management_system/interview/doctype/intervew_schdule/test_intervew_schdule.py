232d1895aebd8b1b44fdb5337e06692273809a5e
Shalindra-coder <shalindraaporiya23@navgurukul.org>
2024-09-18 16:17:22 +0530

    add

---
+++ interview_management_system/interview/doctype/intervew_schdule/test_intervew_schdule.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Shalindra and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestIntervew_schdule(FrappeTestCase):
+	pass
